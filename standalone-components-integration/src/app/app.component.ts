67f94c2448a69d964b4476ec0bc945e03b06bf38
Brian <brian@briankiernan.com>
2023-09-01 16:43:47 -0400

    set intervals on all monitoring properties

--- standalone-components-integration/src/app/app.component.ts
+++ standalone-components-integration/src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { MonitoringService } from './monitoring.service';
+
+import { getRandom } from 'src/utils/get-random';
 
 @Component({
   selector: 'app-root',
   template: ` <app-global-status-bar></app-global-status-bar> `,
   styles: [],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private monitoringService: MonitoringService) {
+    const mutate = this.monitoringService.monitoring.mutate;
+
+    setInterval(() => {
+      mutate((value) => (value.altitude = getRandom()));
+    }, getRandom(4_000, 8_000));
+
+    setInterval(() => {
+      mutate((value) => (value.equipment = getRandom()));
+    }, getRandom(8_000, 12_000));
+
+    setInterval(() => {
+      mutate((value) => (value.thermal = getRandom()));
+    }, getRandom(6_000, 10_000));
+  }
+}
