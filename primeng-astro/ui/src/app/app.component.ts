3d73a5046be734ea5acd8fedb9e1bd39f9029fc8
Brian <brian@briankiernan.com>
2023-10-19 17:44:07 -0400

    code formating on save

--- primeng-astro/ui/src/app/app.component.ts
+++ primeng-astro/ui/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { CommonModule } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
-import { ButtonModule } from "primeng/button";
-import { CardModule } from "primeng/card";
-import { TabViewModule } from "primeng/tabview";
-import { PanelModule } from "primeng/panel";
+import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TabViewModule } from 'primeng/tabview';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
@@ -19,11 +19,11 @@ import { PanelModule } from "primeng/panel";
     TabViewModule,
     PanelModule,
   ],
-  templateUrl: "./app.component.html",
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   constructor(private http: HttpClient) {
-    this.http.get("/api/v1/posts").subscribe(console.log);
+    this.http.get('/api/v1/posts').subscribe(console.log);
   }
 }
