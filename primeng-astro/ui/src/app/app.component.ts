a403146867ed2d19dad5e613c8481da939d837fb
Frances Morrison <frances.morrison@rocketcom.com>
2023-10-19 18:02:58 -0400

    astro tokens(feat): styling button and card in the Astro style with PrimeNg custom theme (#2)
    * feat: adding in primeng custom theme
    
    * feat: styling button and card like Astro using tokens
    
    * add prettierrc
    
    * add BrowserAnimationsModule
    
    * code formating on save
    
    * uncomment panel
    
    * remove node_modules as part of import
    
    ---------
    
    Co-authored-by: Frances Morrison <francesrocket@Rocket-FRANCES.M>
    Co-authored-by: Brian <brian@briankiernan.com>

--- primeng-astro/ui/src/app/app.component.ts
+++ primeng-astro/ui/src/app/app.component.ts
@@ -3,11 +3,22 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TabViewModule } from 'primeng/tabview';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, ButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    ButtonModule,
+    CardModule,
+    TabViewModule,
+    PanelModule,
+  ],
   templateUrl: './app.component.html',
   styles: [],
 })
