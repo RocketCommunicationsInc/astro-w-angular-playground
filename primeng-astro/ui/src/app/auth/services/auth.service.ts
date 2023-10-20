f386f48f90539e4fe2568b3912bdd233af633995
Brian <brian@briankiernan.com>
2023-10-19 23:51:49 -0400

    create auth entity and data extentions

---
+++ primeng-astro/ui/src/app/auth/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {
+  EntityCollectionServiceBase,
+  EntityCollectionServiceElementsFactory,
+} from '@ngrx/data';
+
+import { User, userKey } from '../auth.model';
+
+@Injectable()
+export class AuthService extends EntityCollectionServiceBase<User> {
+  constructor(serviceElementsFactory: EntityCollectionServiceElementsFactory) {
+    super(userKey, serviceElementsFactory);
+  }
+}
