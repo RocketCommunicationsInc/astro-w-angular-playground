a8980b4aaf7da96a4fea84c03614012c37e13b0b
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-10-20 15:08:06 -0400

    feat: working on the gsb

--- primeng-astro/ui/src/app/dashboard/dashboard.component.ts
+++ primeng-astro/ui/src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,19 @@ import { logout, selectUserState } from '../auth/state';
 import { CardModule } from 'primeng/card';
 import { TabViewModule } from 'primeng/tabview';
 import { PanelModule } from 'primeng/panel';
+import { GlobalStatusBarComponent } from '../global-status-bar/global-status-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ButtonModule, CardModule, TabViewModule, PanelModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    CardModule,
+    TabViewModule,
+    PanelModule,
+    GlobalStatusBarComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styles: [],
 })
