82992e9f1caf7738a69d171cb8645f78d934212e
Brian <brian@briankiernan.com>
2023-10-23 10:32:14 -0400

    add initial user login list

--- primeng-astro/ui/src/app/auth/auth.component.ts
+++ primeng-astro/ui/src/app/auth/auth.component.ts
@@ -1,29 +1,43 @@
-import { Observable, noop, tap } from 'rxjs';
+import { noop, tap } from 'rxjs';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { ButtonModule } from 'primeng/button';
+import { ListboxChangeEvent, ListboxModule } from 'primeng/listbox';
 
 import { login } from './state';
-import { User } from './auth.model';
 import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, ListboxModule],
   templateUrl: './auth.component.html',
-  styles: [],
+  styles: [
+    `
+      :host {
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthComponent {
-  users$: Observable<User[]> = this.authService.entities$;
+  users$ = this.authService.entities$;
+  userId = 0;
 
   constructor(private authService: AuthService, private store: Store) {}
 
+  onChange(e: ListboxChangeEvent) {
+    this.userId = e.value;
+  }
+
   login() {
     this.authService
-      .getByKey(3)
+      .getByKey(this.userId)
       .pipe(tap((user) => this.store.dispatch(login({ payload: user }))))
       .subscribe({
         next: noop,
