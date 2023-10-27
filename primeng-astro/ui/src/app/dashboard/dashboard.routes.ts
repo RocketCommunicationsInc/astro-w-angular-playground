175de5b3dbb7b3bd6fd187b5e83507829d3ef99a
Brian <brian@briankiernan.com>
2023-10-27 10:38:09 -0400

    load all user resources on login to dashboard, remove/add some thing on login and logout, add effect to undo optimisitc update error

--- primeng-astro/ui/src/app/dashboard/dashboard.routes.ts
+++ primeng-astro/ui/src/app/dashboard/dashboard.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
 
-import { Path } from '../shared';
+import { Path, loadedEntityResolver } from '../shared';
 import { authGuard } from '../auth/auth.guard';
 import { DashboardComponent } from './dashboard.component';
+import { AlbumsService } from '../albums/albums.service';
+import { PhotosService } from '../photos/photos.service';
+import { PostsService } from '../posts/services';
+import { TodosService } from '../todos/todos.service';
 
 export const dashboardRoutes: Routes = [
   {
     path: Path.dashboard,
     component: DashboardComponent,
     canActivate: [authGuard],
+    resolve: [
+      () => loadedEntityResolver(AlbumsService),
+      () => loadedEntityResolver(PhotosService),
+      () => loadedEntityResolver(PostsService),
+      () => loadedEntityResolver(TodosService),
+    ],
   },
 ];
--- primeng-astro/ui/src/app/posts/posts.routes.ts
+++ primeng-astro/ui/src/app/posts/posts.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 
 import { Path, loadedEntityResolver } from '../shared';
 import { authGuard } from '../auth/auth.guard';
-import { CommentsService, PostsService } from './services';
+import { PostsService } from './services';
 import { PostsComponent } from './posts.component';
 import { PostComponent } from './post/post.component';
+import { postCommentsResolver } from './post-comments.resolver';
 
 export const postsRoutes: Routes = [
   {
@@ -20,7 +21,7 @@ export const postsRoutes: Routes = [
         path: ':postId',
         component: PostComponent,
         resolve: {
-          comments: () => loadedEntityResolver(CommentsService),
+          comments: postCommentsResolver,
         },
       },
     ],
