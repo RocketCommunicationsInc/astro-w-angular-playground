84b98022ba2b50c04efef159ab1202e4dfc212d7
Frances Morrison <francesrocket@Rocket-FRANCES.M>
2023-11-02 09:43:51 -0400

    fix: cutting the amount of photos returned down to 25

--- primeng-astro/ui/src/app/photos/photos.component.ts
+++ primeng-astro/ui/src/app/photos/photos.component.ts
@@ -4,6 +4,7 @@ import { PanelModule } from 'primeng/panel';
 import { CardModule } from 'primeng/card';
 import { PhotosService } from './photos.service';
 import { SkeletonModule } from 'primeng/skeleton';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-photos',
@@ -22,7 +23,9 @@ import { SkeletonModule } from 'primeng/skeleton';
   ],
 })
 export class PhotosComponent {
-  photos$ = this.photosService.entities$;
+  photos$ = this.photosService.entities$.pipe(
+    map((photos) => photos.slice(0, 25)),
+  );
   loading$ = this.photosService.loading$;
   skeletons = Array(2).fill(2);
 
